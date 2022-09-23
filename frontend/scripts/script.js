bb1f38646d3b33ef3ae69753261d0a562fb9f04c
JaafarHawli <jaafarhawle@gmail.com>
2022-09-23 22:35:50 +0300

    added media queries

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -20,10 +20,6 @@ $(function() {
 					.id})">Read more</a>
                 </div>
             </div>`;
-				/* const  = document.getElementById(`${data[i].id}`);
-				i.addEventListener('click', () => {
-					console.log(i);
-				});  */
 			}
 		}
 	});
@@ -43,7 +39,8 @@ function openPage(id) {
             <h1 class="pt-3">
                 ${data[0].title}
             </h1>
-            <p class="pt-3">
+            <p class="fw-bold fs-5">By: ${data[0].author}</p>
+            <p class="pt-3 margin">
                 ${data[0].content}
             </p>`;
 		}
@@ -56,39 +53,3 @@ const quit = () => {
 	article.classList.add('d-none');
 	body.classList.remove('d-none');
 };
-/* newsElement.forEach((card) => card.addEventListener('click', openPage));
-
-function openPage() {
-    article.classList.remove('.d-none');
-    body.classList.add('.d-none');
-    $(function() {
-        $.ajax({
-            dataType: 'json',
-            type: 'GET',
-            url: `http://localhost/news-website-backend/view-article.php?=id=${card}`,
-            success: function(data) {
-                for (let i in data) {
-                    newsGrid.innerHTML += `<div class="card" style="width: 18rem;">
-                    <img src="${data[i].image_url}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">${data[i].title}</h5>
-                      <p class="card-text">${data[i].subtitle}</p>
-                      <a href="#" class="news-items btn btn-warning" id=${data[i].id}>Read more</a>
-                    </div>
-                </div>`;
-                }
-            }
-        });
-    });
-
-
-    article.innerHTML = `<img src="./images/48899.jpg" alt="..." class="article-img pt-4">
-    <h1 class="pt-3">
-        Everything you ever need to know about flowers
-    </h1>
-    <p class="pt-3">
-        Ennui single-origin coffee crucifix, irony PBR squid sriracha. Kale chips slow-carb gastropub DIY, wayfarers Williamsburg lo-fi distillery +1 kitsch tofu Marfa beard. Truffaut food truck meh seitan, Etsy Wes Anderson Odd Future. Godard Tumblr lomo Intelligentsia cliche hoodie. Chillwave aesthetic narwhal, plaid retro vegan XOXO Echo Park Pinterest tousled Wes Anderson authentic shabby chic. Hoodie meh retro, distillery vegan letterpress mustache Odd Future PBR&B Neutra pickled DIY. Next level mlkshk ennui fashion axe.
-    </p>`
-
-}
- */
